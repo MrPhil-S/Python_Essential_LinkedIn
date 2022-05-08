00a6ffab89fe1ac9c89fa013dd81046ea5b8c594
MrPhil-S <pschims@gmail.com>
2022-05-08 15:36:40 -0700

    Chap 3 files

---
+++ types.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 7
+print('x is {}'.format(x))
+print(f'x is {x}')
+
+#what is the data type of the variable x
+print(type(x))
+
+y = 'word'
+print(type(y))
+
+z = 3.0
+print(type(z))
+
+#stings on multiple lines
+w = '''
+one
+two
+three'''
+print(w)
